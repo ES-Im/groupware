8cc65bb1aa2db1c670a436a135292ee2c0b6295c
eunse <eunseokim43@gmail.com>
2026-08-11 12:00:56 +0900

    - 사원의 부서이력 api 추가

--- back/src/main/java/com/haruon/groupware/adapter/webapi/employee/account/EmpApi.java
+++ back/src/main/java/com/haruon/groupware/adapter/webapi/employee/account/EmpApi.java
@@ -3,12 +3,15 @@ package com.haruon.groupware.adapter.webapi.employee.account;
 import com.haruon.groupware.application.employee.account.provided.forCommand.EmpAccountManager;
 import com.haruon.groupware.application.employee.account.provided.forRetriever.EmpAccountRetriever;
 import com.haruon.groupware.application.employee.account.service.command.dto.EmpRegisterRequest;
+import com.haruon.groupware.application.employee.account.service.query.dto.BelongingInfo;
 import com.haruon.groupware.application.employee.account.service.query.dto.EmpInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -32,6 +35,15 @@ public class EmpApi {
             @PathVariable Long empId
     ) {
         EmpInfoResponse response = retriever.retrieveEmpAccountInfo(empId);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{empId}/belongings")
+    public ResponseEntity<List<BelongingInfo>> getEmpBelongings(
+            @PathVariable Long empId
+    ) {
+        List<BelongingInfo> response = retriever.retrieveEmpBelongingsInfo(empId);
 
         return ResponseEntity.ok().body(response);
     }
